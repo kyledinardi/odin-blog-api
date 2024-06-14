ac6b49810e07c0b34c35ce8dbece861a9b83ad29
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-06-14 15:49:03 -0400

    Remove view templates

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,6 @@ async function main() {
 
 main().catch((err) => debug(err));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
