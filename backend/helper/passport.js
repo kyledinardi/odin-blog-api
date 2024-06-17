ade0ba29c140973594408502f3401824b2779065
kyledinardi <94251418+kyledinardi@users.noreply.github.com>
2024-06-17 12:29:07 -0400

    Add passport jwt strategy

--- backend/helper/passport.js
+++ backend/helper/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
-const LocalStragetgy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
+const LocalStragetgy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 const User = require('../models/user');
 
 passport.use(
@@ -22,6 +24,23 @@ passport.use(
         return done(null, user);
       } catch (err) {
         return done(err);
+      }
+    },
+  ),
+);
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await User.findById(jwtPayload.id);
+        return done(null, user || false);
+      } catch (err) {
+        return done(err, false);
       }
     },
   ),
